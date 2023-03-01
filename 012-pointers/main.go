ce09a7d89014e81d51038fa89ac020fd3600f4a9
Fatih Şen <fatihydrm@outlook.com>
2023-03-01 21:09:53 +0300

    learn: pointer

--- 012-pointers/main.go
+++ 012-pointers/main.go
@@ -1,5 +1,22 @@
 package main
 
-func main(){
+import "fmt"
 
-} 
+// fonksiyona name nin hafızadaki kopyalanmış adresini gönderip değerini değiştiriyoruz
+func updateName(x *string){
+	*x = "Test Name"
+}
+
+func main() {
+	name := "Fatih"
+	
+	// pointer ile name nin hafızadaki adresini kopyalıyoruz
+	m := &name
+	fmt.Println("Memory address: ", m)
+	fmt.Println("Value ot memory address: ", *m)
+
+	// namenin hafızadaki yerini parametre olarak veriyoruz
+	updateName(m)
+
+	fmt.Println(name)
+}
