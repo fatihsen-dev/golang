8cd862dd39b7ea35df35673eabc77ec84a8c33a4
Fatih Şen <fatihydrm@outlook.com>
2023-03-01 23:54:06 +0300

    add: add learning comment

--- 014-receiver-functions/bill.go
+++ 014-receiver-functions/bill.go
@@ -2,6 +2,7 @@ package main
 
 import "fmt"
 
+// struct oluşturma
 type bill struct {
 	name  string
 	items map[string]float64
@@ -18,15 +19,22 @@ func newBill(name string) bill {
 	return b
 }
 
+// struct a tanımlı fonksiyon oluşturma
 func (b bill) format() string {
 	fs := "Bill breakedown \n"
+
+	// toplayıcı değişken
 	var total float64 = 0
 
+	// structs tan türetilen b objesi içerisindeki itemsleri dönüp toplamak için for kullanıyoruz
 	for k, v := range b.items {
+
+		// 
 		fs += fmt.Sprintf("%-25v ...$%v \n",k+":",v)
 		total += v
 	}
 
+	// %0.2f değişkenin ondalıklı kısmının uzunluğunu belirtir 
 	fs += fmt.Sprintf("%-25v ...$%0.2f","Total:",total)
 
 	return fs
