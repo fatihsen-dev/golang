632c41f4f0c2877f9ca048faf21ebfa064a9764f
Fatih Şen <fatihydrm@outlook.com>
2023-03-05 20:04:21 +0300

    feat: golang development

---
+++ golang-development/001-creating-a-simple-restful-api/main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+type Article struct{
+	Title string `json:"title"`
+	Desc string `json:"desc"`
+	Content string `json:"content"`
+}
+
+type Articles []Article
+
+func allArticles(w http.ResponseWriter,r *http.Request){
+	articles := Articles{
+		Article{Title: "Test title",Desc: "Test description",Content: "Hello Word"},
+	}
+
+	fmt.Println("Endpoint Hit: All Articles Endpoint")
+	json.NewEncoder(w).Encode(articles)
+}
+
+func testPostArticles(w http.ResponseWriter, r *http.Request){
+	fmt.Fprintf(w,"Test POST endpoint worked")
+}
+
+func homePage(w http.ResponseWriter, r *http.Request){
+	fmt.Fprint(w,"Golang REST-full Api")
+}
+
+func handleRequests(){
+	myRouter := mux.NewRouter().StrictSlash(true)
+	myRouter.HandleFunc("/",homePage)
+	myRouter.HandleFunc("/articles",allArticles).Methods("GET")
+	myRouter.HandleFunc("/articles",testPostArticles).Methods("POST")
+	log.Fatal(http.ListenAndServe(":5000",myRouter))
+}
+
+func main() {
+	handleRequests()
+}
