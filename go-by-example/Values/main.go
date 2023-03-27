6f0336b34ddbcc3de946781d0436d1566a062290
Fatih Şen <fatihydrm@outlook.com>
2023-03-28 00:46:22 +0300

    Values

---
+++ go-by-example/Values/main.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("go" + "lang")        // golang
+	fmt.Println("1+1 =", 1+1)         // 2
+	fmt.Println("7.0/3.0 =", 7.0/3.0) // 2.3333333333333335
+	fmt.Println(true && false)        // false
+	fmt.Println(true || false)        // true
+	fmt.Println(!true)                // false
+}
